9af093b2fece7e545a69a2c391094f7328dc39a6
Isaak Nicolas <isaak.nicolas@epitech.eu>
2023-12-01 20:23:49 +0100

    platform specific installation

--- install.cmake
+++ install.cmake
@@ -7,19 +7,21 @@ install(
 
 # ---------------------------------- cpack --------------------------------- 
 
-set(CPACK_GENERATOR NSIS)
+if (WIN32)
+    set(CPACK_GENERATOR NSIS)
 
-set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_NAME} - Recursively search and remove files duplicate in the current directory and subdirectories")
+    set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+    set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${PROJECT_NAME} - Recursively search and remove files duplicate in the current directory and subdirectories")
 
-set(CPACK_PACKAGE_VERSION "1.0.0")
-set(CPACK_PACKAGE_VERSION_MAJOR "1")
-set(CPACK_PACKAGE_VERSION_MINOR "0")
-set(CPACK_PACKAGE_VERSION_PATCH "0")
+    set(CPACK_PACKAGE_VERSION "1.0.0")
+    set(CPACK_PACKAGE_VERSION_MAJOR "1")
+    set(CPACK_PACKAGE_VERSION_MINOR "0")
+    set(CPACK_PACKAGE_VERSION_PATCH "0")
 
-set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
 
-set(CPACK_PACKAGE_VENDOR "Helvegen")
-SET(CPACK_NSIS_MODIFY_PATH ON)
+    set(CPACK_PACKAGE_VENDOR "Helvegen")
+    set(CPACK_NSIS_MODIFY_PATH ON)
 
-include(CPack)
+    include(CPack)
+endif()
