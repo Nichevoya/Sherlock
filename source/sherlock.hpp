5b54f5a97909e6dd2e02096fce05127f24446279
Isaak Nicolas <isaal.nicolas@epitech.eu>
2023-12-02 09:37:12 +0100

    comments

--- source/sherlock.hpp
+++ source/sherlock.hpp
@@ -9,9 +9,31 @@ class sherlock {
         sherlock(const std::string &directory = ".");
         ~sherlock() = default;
 
+        /**
+         * @brief Remove corresponding files
+         * 
+         */
         void remove(void);
+        
+        /**
+         * @brief Get directory path
+         * 
+         * @return const std::string &
+         */
         const std::string &directory(void);
+        
+        /**
+         * @brief Get found file list
+         * 
+         * @return std::vector<std::string> & 
+         */
         std::vector<std::string> &files(void);
+        
+        /**
+         * @brief Scan given directory and fill the file list
+         * 
+         * @return const std::vector<std::string> & 
+         */
         const std::vector<std::string> &scan(void);
 
     protected:
